0e3bc594ec1571c2033b8f0c9f854f392e8f5f44
ak tech <aktech1616@gmail.com>
2021-07-28 10:52:26 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+require('./db/conn.js');
+app.use(require('./router/signup.js'));
+app.use(require('./router/confirmation.js'));
+app.use(require('./router/login.js'));
+app.use(require('./router/about.js'));
+app.use(require('./router/updateinfo.js'));
+app.use(require('./router/delete_acc.js'))
+
+
+const PORT =process.env.PORT || 5000
+if(process.env.NODE_ENV ="production"){
+    app.use(express.static("insta/build"))
+}
+
+
+console.log('app started')
+app.listen(PORT)
